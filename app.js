c20d6a6a8a6d606cc0aed54d0f5033645dac95cf
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-05 17:53:40 +0200

    creating player and game objects

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ window.addEventListener('load', function() {
 
     }
     class Player {
-
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+          context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
 
@@ -31,6 +44,18 @@ window.addEventListener('load', function() {
 
     }
     class Game {
-        
+       constructor(width, height) {
+          this.width = width;
+          this.height = height;
+          this.pplayer = new Player(this)
+       }
+       update() {
+          this.player.update();
+       }
+       draw(context) {
+          this.player.draw(context);
+       }
     }
+
+    const game = new Game(canvas.width, canvas.height)
 })
