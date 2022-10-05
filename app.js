4a3f291f7cecfcae34c7cd35de5319a434e07b8f
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-05 21:27:13 +0200

    creating projectiles

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ window.addEventListener('load', function() {
                       (e.key === 'ArrowDown')
             ) && this.game.keys.indexOf(e.key) === -1 ) {
                     this.game.keys.push(e.key);
+                } else if (e.key === ' ') {
+                    this.game.player.shootTop();
                 }
                 console.log(this.game.keys)
             });
@@ -22,13 +24,30 @@ window.addEventListener('load', function() {
                 if(this.game.keys.indexOf(e.key) > -1) {
                     this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
                 }
-                console.log(this.game.keys)
+                
             })
         }
     }
     class Projectile {
-
+        constructor(game, x, y) {
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 3; 
+            this.markedForDeletion = false;
+        }
+        update(){
+            this.x += this.speed;
+            if (this.x > this.game.width * 0,8) this.markedForDeletion = true;
+        }
+        draw(context) {
+            context.fillStyle = 'yellow';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
+    
     class Particle {
 
     }
@@ -41,15 +60,29 @@ window.addEventListener('load', function() {
             this.y = 100;
             this.speedY = 0;
             this.maxSpeed = 2;
+            this.projectiles = [];
         }
         update() {
             if (this.game.keys.includes('ArrowUp'))  this.speedY = -this.maxSpeed;
             else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             else this.speedY = 0;
             this.y += this.speedY;
+            // handle projectiles
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
         }
         draw(context) {
+          context.fillStyle = "black";
           context.fillRect(this.x, this.y, this.width, this.height);
+          this.projectiles.forEach(projectile => {
+            projectile.draw(context);
+        });
+        }
+        shootTop() {
+            this.projectiles.push(new Projectile(this.game, this.x, this.y));
+            console.log(this.projectiles);
         }
     }
     class Enemy {
