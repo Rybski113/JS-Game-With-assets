a8e59859775d0feca195fbd002ac6b29c540e00e
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-05 16:49:37 +0200

    Basic JavaScript setup
    Object Oriented  programming with JS

---
+++ app.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+    // canvas setup
+
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class Inputhandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+        
+    }
+})
