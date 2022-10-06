91e40e171a40f1d7c08b9cf88e8a03eb2f93ea0d
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-06 20:53:25 +0200

    Counting game time

--- app.js
+++ app.js
@@ -140,10 +140,12 @@ window.addEventListener('load', function() {
             //score
             context.fillText('Score: ' + this.game.score, 20, 40)
             //ammo
-            
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            //timer
+            const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+            context.fillText('Timer: ' + formattedTime, 20, 100);
             //game over messages
             if (this.game.gameOver) {
                 context.textAlign = 'center';
@@ -154,7 +156,7 @@ window.addEventListener('load', function() {
                     message2 = 'Well Done';
                 } else {
                     message1 = 'You Lose!';
-                    message1 = 'Try Again Next Time!';
+                    message2 = 'Try Again Next Time!';
                 }
                 context.font = '50px' + this.fontFamily;
                 context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 40);
@@ -182,8 +184,12 @@ window.addEventListener('load', function() {
           this.gameOver = false;
           this.score = 0;
           this.winningScore = 10;
+          this.gameTime = 0;
+          this.timeLimit = 5000;
        }
        update(deltaTime) {
+          if(!this.gameOver) this.gameTime += deltaTime;
+          if (this.gameTime > this.timeLimit) this.gameOver = true;
           this.player.update();
           if (this.ammoTimer > this.ammoInterval){
             if(this.ammo < this.maxAmmo) this.ammo ++;
@@ -202,7 +208,7 @@ window.addEventListener('load', function() {
                     projectile.markedForDeletion = true;
                     if (enemy.lives <= 0) {
                         enemy.markedForDeletion = true;
-                        this.score += enemy.score;
+                        if(!this.gameOver) this.score += enemy.score;
                         if(this.score > this.winningScore) this.gameOver = true;
                     }
                 }
