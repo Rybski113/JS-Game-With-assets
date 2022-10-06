a0de31604575428f1be416969bf481153252b63c
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-10-06 19:50:54 +0200

    game score added

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ window.addEventListener('load', function() {
          this.x = this.game.width;
          this.speedX = Math.random() * -2.5 - -0.5;
          this.markedForDeletion = false;
-         this.lives = 5;
+         this.lives = 3;
          this.score = this.lives;
 
        }
@@ -131,11 +131,20 @@ window.addEventListener('load', function() {
             this.color = 'white';
          }
          draw(context){
-            //ammo
+            context.save();
             context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor = 'black';
+            context.font = this.fontSize + 'px' + this.fontFamily;
+            //score
+            context.fillText('Score: ' + this.game.score, 20, 40)
+            //ammo
+            
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            context.restore();
          }
     }
     class Game {
@@ -149,11 +158,13 @@ window.addEventListener('load', function() {
           this.enemies = [];
           this.enemyTimer = 0;
           this.enemyInterval = 1000;
-          this.ammo = 20;
+          this.ammo = 30;
           this.maxAmmo = 50;
           this.ammoTimer = 0;
           this.ammoInterval = 500;
           this.gameOver = false;
+          this.score = 0;
+          this.winningScore = 10;
        }
        update(deltaTime) {
           this.player.update();
@@ -175,6 +186,7 @@ window.addEventListener('load', function() {
                     if (enemy.lives <= 0) {
                         enemy.markedForDeletion = true;
                         this.score += enemy.score;
+                        if(this.score > this.winningScore) this.gameOver = true;
                     }
                 }
             })
